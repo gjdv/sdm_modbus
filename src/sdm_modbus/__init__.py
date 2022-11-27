bbd273c4b3cb0eb2ff1afa9e446b813536ed9780
gjdv <gjdv@hotmail.com>
2022-11-27 19:45:05 +0100

    adjusted __init__.py for direct use from subfolder

--- src/sdm_modbus/__init__.py
+++ src/sdm_modbus/__init__.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+sdm_modbus_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+try:
+    sys.path.index(sdm_modbus_dir)
+except ValueError:
+    print("Added sdm_modbus to python path")
+    sys.path.append(sdm_modbus_dir)
+
 from sdm_modbus.meter import *
 from sdm_modbus.sdm import *
 from sdm_modbus.garo import *
